b504c73a56ffbd35a116a94f303d1245238c29ad
JoeD <daveyj@purdue.edu>
2019-04-23 14:56:59 -0400

    Bird Sound Works (Still need to tweak it)
    Sound is a bit loud and overwhelming

--- FinalProjectStuff/Assets/Scripts/Bird.cs
+++ FinalProjectStuff/Assets/Scripts/Bird.cs
@@ -4,18 +4,25 @@ using UnityEngine;
 
 public class Bird : MonoBehaviour
 {
-
+    private float killDistance = 20000;
     private Animator anim;
+    private Vector3 startPos;
+
     // Start is called before the first frame update
     void Start()
     {
         anim = gameObject.GetComponent<Animator>();
         anim.Play("fly");
+        startPos = transform.position;
     }
 
     // Update is called once per frame
     void Update()
     {
         transform.position += Vector3.forward * Time.deltaTime * 1;
+        if (Vector3.Distance(startPos, transform.position) > killDistance)
+        {
+            Destroy(gameObject);
+        }
     }
 }

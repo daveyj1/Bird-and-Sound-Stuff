3fb8f22df82133d570b590fac01c2cb5adaf3fc1
JoeD <daveyj@purdue.edu>
2019-04-23 14:31:27 -0400

    Bird and Spawnder Work

---
+++ FinalProjectStuff/Assets/Scripts/Bird.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bird : MonoBehaviour
+{
+
+    private Animator anim;
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = gameObject.GetComponent<Animator>();
+        anim.Play("fly");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position += Vector3.forward * Time.deltaTime * 1;
+    }
+}
---
+++ FinalProjectStuff/Assets/Scripts/BirdSpawner.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BirdSpawner : MonoBehaviour
+{
+    public GameObject bird; //Bird Prefab
+    public float frequency; //How often a bird will spawn in seconds
+    public float startTime; //After how many seconds the first bird will spawn
+    private float beginTime; //Time when script starts
+    private float lastTime; //Time of last spawn
+
+    void Start()
+    {
+        beginTime = Time.time;
+        lastTime = Time.time;
+    }
+    
+    void Update()
+    {
+        if (beginTime + startTime < Time.time)
+        {
+            if (lastTime + frequency < Time.time)
+            {
+                Instantiate(bird, transform.position, Quaternion.identity);
+                lastTime = Time.time;
+            }
+        }
+    }
+}
